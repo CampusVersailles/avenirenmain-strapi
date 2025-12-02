7ae699a18acdcb855f082103686002385522d4f8
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2025-12-02 10:48:11 +0100

    fix: formations filter (#15)

--- src/api/formation/controllers/formation.ts
+++ src/api/formation/controllers/formation.ts
@@ -7,7 +7,10 @@ import Fuse, { FuseResult } from "fuse.js";
 
 const DEFAULT_RADIUS_KM = 20;
 const TARGET_UID = "api::formation.formation";
-type Formation = Data.ContentType<typeof TARGET_UID>;
+type Formation = Data.ContentType<typeof TARGET_UID> & {
+  score?: number;
+  distance?: number;
+};
 
 const calculateDistance = (
   lat1: number,
@@ -44,9 +47,11 @@ export default factories.createCoreController(
         ? parseFloat(radius as string)
         : DEFAULT_RADIUS_KM;
 
-      const page = parseInt((otherQuery["pagination[page]"] as string) || "1");
+      // @ts-expect-error pagination type
+      const page = parseInt((otherQuery.pagination.page as string) || "1");
       const pageSize = parseInt(
-        (otherQuery["pagination[pageSize]"] as string) || "25"
+        // @ts-expect-error pagination type
+        (otherQuery.pagination.pageSize as string) || "25"
       );
 
       const baseFilters = otherQuery.filters || {};
@@ -67,37 +72,67 @@ export default factories.createCoreController(
       if (fuzzy) {
         const fuse = new Fuse(lightFormations, {
           keys: ["titre"],
-          threshold: 0.4,
+          threshold: 0.25,
+          isCaseSensitive: false,
+          includeScore: true,
         });
 
         const searchResults = fuse.search(
           fuzzy as string
         ) as FuseResult<Formation>[];
-        lightFormations = searchResults.map((result) => result.item);
+        lightFormations = searchResults.map((result) => ({
+          ...result.item,
+          score: result.score,
+        }));
       }
 
       let filteredFormations = lightFormations;
       if (lat !== null && lng !== null) {
-        filteredFormations = lightFormations.filter((formation) => {
-          if (!formation.adresse?.latitude || !formation.adresse?.longitude) {
-            return false;
-          }
-          const distance = calculateDistance(
-            lat,
-            lng,
-            formation.adresse.latitude,
-            formation.adresse.longitude
+        filteredFormations = lightFormations
+          .map((formation) => {
+            if (!formation.adresse?.latitude || !formation.adresse?.longitude) {
+              return null;
+            }
+            const distance = calculateDistance(
+              lat,
+              lng,
+              formation.adresse.latitude,
+              formation.adresse.longitude
+            );
+            return { ...formation, distance };
+          })
+          .filter(
+            (formation) => formation !== null && formation.distance! <= radiusKm
           );
-          return distance <= radiusKm;
-        });
       }
 
       const total = filteredFormations.length;
       const start = (page - 1) * pageSize;
       const end = start + pageSize;
       const paginatedIds = filteredFormations
+        .sort((a, b) => {
+          if (a.score !== undefined && b.score !== undefined) {
+            if (a.score !== b.score) {
+              return a.score - b.score;
+            }
+          } else if (a.score !== undefined) {
+            return -1;
+          } else if (b.score !== undefined) {
+            return 1;
+          }
+          if (a.distance !== undefined && b.distance !== undefined) {
+            if (a.distance !== b.distance) {
+              return a.distance - b.distance;
+            }
+          } else if (a.distance !== undefined) {
+            return -1;
+          } else if (b.distance !== undefined) {
+            return 1;
+          }
+          return a.titre.localeCompare(b.titre);
+        })
         .slice(start, end)
-        .map((f: any) => f.id);
+        .map((formation) => formation.id);
 
       const fullFormations = await strapi.entityService.findMany(
         "api::formation.formation",
