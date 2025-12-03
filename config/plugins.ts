e020434517a292bc9d8fa052b4117c41c2c62a74
Paul Bernard <bernardp.imag@gmail.com>
2025-12-03 11:36:07 +0100

    Enable s3-compatible scaleway for media uploads in Strapi. +configure strapi midleware to display thumbnails.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,25 @@
-export default () => ({});
+export default ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          credentials: {
+            accessKeyId: env('SCALEWAY_ACCESS_KEY_ID'),
+            secretAccessKey: env('SCALEWAY_ACCESS_SECRET'),
+          },
+          endpoint: env('SCALEWAY_S3_ENDPOINT'),
+          region: env('SCALEWAY_REGION', 'fr-par'),
+          forcePathStyle: true,
+          params: {
+            ACL: env('SCALEWAY_ACL', 'public-read'),
+            Bucket: env('SCALEWAY_BUCKET'),
+          },
+        },
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    },
+  });
