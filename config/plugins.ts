9641ff93113bc777ad508f10e87f77871de4044c
Paul Bernard <bernardp.imag@gmail.com>
2025-12-03 15:04:39 +0100

    Add option to switch between local or scaleway based on `UPLOAD_PROVIDER` env variable.

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,25 +1,41 @@
-export default ({ env }) => ({
-    upload: {
-      config: {
-        provider: 'aws-s3',
-        providerOptions: {
-          credentials: {
-            accessKeyId: env('SCALEWAY_ACCESS_KEY_ID'),
-            secretAccessKey: env('SCALEWAY_ACCESS_SECRET'),
+export default ({ env }) => {
+  const provider = env("UPLOAD_PROVIDER", "local");
+
+  if (provider === "scaleway") {
+    return {
+      upload: {
+        config: {
+          provider: "aws-s3",
+          providerOptions: {
+            credentials: {
+              accessKeyId: env("SCALEWAY_ACCESS_KEY_ID"),
+              secretAccessKey: env("SCALEWAY_ACCESS_SECRET"),
+            },
+            endpoint: env("SCALEWAY_S3_ENDPOINT"),
+            region: env("SCALEWAY_REGION", "fr-par"),
+            forcePathStyle: true,
+            params: {
+              ACL: env("SCALEWAY_ACL", "public-read"),
+              Bucket: env("SCALEWAY_BUCKET"),
+            },
           },
-          endpoint: env('SCALEWAY_S3_ENDPOINT'),
-          region: env('SCALEWAY_REGION', 'fr-par'),
-          forcePathStyle: true,
-          params: {
-            ACL: env('SCALEWAY_ACL', 'public-read'),
-            Bucket: env('SCALEWAY_BUCKET'),
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
           },
-        },
-        actionOptions: {
-          upload: {},
-          uploadStream: {},
-          delete: {},
         },
       },
+    };
+  }
+
+  // Local provider by default
+  return {
+    upload: {
+      config: {
+        provider: "local",
+        providerOptions: {},
+      },
     },
-  });
+  };
+};
