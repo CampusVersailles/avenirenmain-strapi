e93d1dbca78b5098f34d15fce1ddc1b5914e1188
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-01-12 08:35:52 +0100

    feat: add partenaires (#32)

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -666,6 +666,48 @@ export interface ApiOrientationOrientation extends Struct.SingleTypeSchema {
   };
 }
 
+export interface ApiPartenairePartenaire extends Struct.CollectionTypeSchema {
+  collectionName: 'partenaires';
+  info: {
+    displayName: 'Partenaire';
+    pluralName: 'partenaires';
+    singularName: 'partenaire';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    createdAt: Schema.Attribute.DateTime;
+    createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+    description: Schema.Attribute.Blocks;
+    locale: Schema.Attribute.String & Schema.Attribute.Private;
+    localizations: Schema.Attribute.Relation<
+      'oneToMany',
+      'api::partenaire.partenaire'
+    > &
+      Schema.Attribute.Private;
+    logo: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios',
+      true
+    >;
+    nom: Schema.Attribute.String;
+    publishedAt: Schema.Attribute.DateTime;
+    site: Schema.Attribute.String;
+    type: Schema.Attribute.Enumeration<
+      [
+        'B\u00E2tisseur',
+        'Financeur',
+        'Concepteur et relecteur',
+        'Institutionnel et ressource',
+      ]
+    >;
+    updatedAt: Schema.Attribute.DateTime;
+    updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
+      Schema.Attribute.Private;
+  };
+}
+
 export interface PluginContentReleasesRelease
   extends Struct.CollectionTypeSchema {
   collectionName: 'strapi_releases';
@@ -1182,6 +1224,7 @@ declare module '@strapi/strapi' {
       'api::formation.formation': ApiFormationFormation;
       'api::metier.metier': ApiMetierMetier;
       'api::orientation.orientation': ApiOrientationOrientation;
+      'api::partenaire.partenaire': ApiPartenairePartenaire;
       'plugin::content-releases.release': PluginContentReleasesRelease;
       'plugin::content-releases.release-action': PluginContentReleasesReleaseAction;
       'plugin::i18n.locale': PluginI18NLocale;
