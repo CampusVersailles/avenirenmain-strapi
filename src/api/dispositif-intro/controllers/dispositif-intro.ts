ef66e9b9b04b909284135bd75b77492de4d32042
Xavier Desoindre <xavier.desoindre@hotmail.fr>
2026-09-11 18:05:06 +0200

    feat: add dispositif intro

---
+++ src/api/dispositif-intro/controllers/dispositif-intro.ts
@@ -0,0 +1,9 @@
+/**
+ * dispositif-intro controller
+ */
+
+import { factories } from "@strapi/strapi";
+
+export default factories.createCoreController(
+  "api::dispositif-intro.dispositif-intro",
+);
